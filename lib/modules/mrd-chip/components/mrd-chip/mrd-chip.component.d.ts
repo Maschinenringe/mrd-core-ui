483ca16b862c9db0c25326e329feb0941eff14a6
Benjamin Klein <bk@fub.de>
2025-02-11 11:55:30 +0100

    ChipSelection Disable Fix, DateInput Control Fix

--- lib/modules/mrd-chip/components/mrd-chip/mrd-chip.component.d.ts
+++ lib/modules/mrd-chip/components/mrd-chip/mrd-chip.component.d.ts
@@ -5,11 +5,12 @@ export declare class MrdChipComponent implements AfterViewInit {
     chipText: ElementRef<HTMLElement> | undefined;
     prefixIcon: string;
     suffixIcon: string;
+    disabled: boolean;
     close: EventEmitter<void>;
     chipTextValue: string;
     constructor(cdr: ChangeDetectorRef);
     ngAfterViewInit(): void;
     closeClicked(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<MrdChipComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<MrdChipComponent, "mrd-chip", never, { "prefixIcon": { "alias": "prefixIcon"; "required": false; }; "suffixIcon": { "alias": "suffixIcon"; "required": false; }; }, { "close": "close"; }, never, ["*"], false, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<MrdChipComponent, "mrd-chip", never, { "prefixIcon": { "alias": "prefixIcon"; "required": false; }; "suffixIcon": { "alias": "suffixIcon"; "required": false; }; "disabled": { "alias": "disabled"; "required": false; }; }, { "close": "close"; }, never, ["*"], false, never>;
 }
